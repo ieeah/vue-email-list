a8559618b8324ab85789d9af51c3b854855ccc44
ieeah <lbnfnc90@gmail.com>
2021-11-19 16:43:42 +0100

    git end

--- main.js
+++ main.js
@@ -2,13 +2,19 @@ const app = new Vue({
     el: '#app',
     data: {
         emails: [],
+        display: '',
     },
-    created() {},
+    computed: {
+        emailsAreTen() {
+            return this.emails.length > 9 ? true : false;
+        },
+    },
     methods: {
         get10Mails() {
             for (let i = 1; i < 11; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
                     console.log(response.data.response);
+                    this.emails.push(response.data.response);
                 });
             }
         }
